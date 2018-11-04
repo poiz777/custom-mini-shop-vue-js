eb4930b8e0c4d4b97e3fd25ca8637461c6a6d2e1
Poiz <seesolutionz@gmail.com>
2018-11-04 22:14:04 +0100

    Wrap Items within Group-Blocks to ensure Uniformity of Spacing...

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,106 +1,117 @@
 <template >
     <div class="container" >
-      <div style="margin:0 auto;padding:40px;text-align:center;display:block;color:#dadada;" v-if="!rendered">Loading...</div>
-      <div class="fa fa-5x fa-cog fa-spin" style="margin:0 auto;padding:40px;text-align:center;display:block;color:#dadada;" v-if="!rendered" ></div>
-      <div style="clear:both"></div>
-      <Menu @currentRouteChanged="updateRoute"></Menu>
-      <div style="clear:both"></div>
-      <section  class="pz-wrapper-block pz-col-12 pz-cue-box"   >
-          <app-item v-for="(productItem, cueKey) in itemChunks"
-                  :dataIndex="productItem.id"
-                  :key="getUniqueKey(cueKey)"
-                  :wrapContent="true"
-                  :prodItem="productItem"
-                  :cue="cueKey"
-                  :colNum="getColNum()">
-          </app-item>
-      </section>
+        <div style="margin:0 auto;padding:40px;text-align:center;display:block;color:#dadada;" v-if="!rendered">Loading...</div>
+        <div class="fa fa-5x fa-cog fa-spin" style="margin:0 auto;padding:40px;text-align:center;display:block;color:#dadada;" v-if="!rendered" ></div>
+        <div style="clear:both"></div>
+        <Menu @currentRouteChanged="updateRoute"></Menu>
+        <div style="clear:both"></div>
+        <article :class="'pz-wrapper-block pz-col-12 pz-group-block-'+cueKey" v-for="(itemsGroup, cueKey) in itemChunks" v-bind:key="getUniqueKey(cueKey)" >
+            <app-item
+                    v-for="(productItem, index) in itemsGroup"
+                    :dataIndex="productItem.id"
+                    :key="getUniqueKey(productItem.id)"
+                    :wrapContent="true"
+                    :prodItem="productItem"
+                    :cue="index"
+                    :colNum="getColNum()">
+            </app-item>
+        </article>
     </div>
 </template>
 
 <script>
-    import Item       from "./Item";
-    import sMeths     from "@/Services/SharedMethods";
-    import Menu       from "./Menu";
-    import ajaxFetch  from '@/Services/JQAjax';
-    export default {
-        name: "Items",
-        props: ['categories'],
+	import Item       from "./Item";
+	import sMeths     from "@/Services/SharedMethods";
+	import Menu       from "./Menu";
+	import ajaxFetch  from '@/Services/JQAjax';
+	export default {
+		name: "Items",
+		props: ['categories'],
 
-        data: function(){
-          return {
-              psm: ajaxFetch.psm,
-              preLoaderURI: '/assets/images/icons/pz-loader-05.gif',
-              itemChunks: {},
-              rendered : false,
-              productItems: [],
-              columnsPerRow:4,
-              colsPerRow:4,
-              prodCount  : 0,
-              headerString  : '',
-              detailPageData  : {},
-          }
-        },
+		data: function(){
+			return {
+				psm: ajaxFetch.psm,
+				preLoaderURI: '/assets/images/icons/pz-loader-05.gif',
+				itemChunks: [],
+				rendered : false,
+				productItems: [],
+				columnsPerRow:4,
+				colsPerRow:4,
+				prodCount  : 0,
+				headerString  : '',
+				detailPageData  : {},
+			}
+		},
 
-        created: function(){
-            this.getChunks();
-        },
+		created: function(){
+			this.getChunks();
+		},
 
-        computed: {
-            nullV: {
-                get: function(){
-                    return this.cid;
-                },
-                set: function(){},
-            }
-        },
-        watch: {
-            url: function () {
-                this.currentRoute =  this.$route.params.catID || 1;
-                this.$emit('currentRouteChanged', this.currentRoute);
-            },
-        },
+		computed: {
+			nullV: {
+				get: function(){
+					return this.cid;
+				},
+				set: function(){},
+			}
+		},
+		watch: {
+			url: function () {
+				this.currentRoute =  this.$route.params.catID || 1;
+				this.$emit('currentRouteChanged', this.currentRoute);
+			},
+		},
 
-        components : {
-          'app-item' : Item,
-          'Menu'  : Menu,
-        },
+		components : {
+			'app-item' : Item,
+			'Menu'  : Menu,
+		},
 
-        methods : {
-            ...sMeths,
-            ...{
-                updateRoute: function(route) {
-                    this.cid = route;
-                    this.getChunks();
-                    this.$emit('cidChanged', route);
-                },
+		methods : {
+			...sMeths,
+			...{
+				updateRoute: function(route) {
+					this.cid = route;
+					this.getChunks();
+					this.$emit('cidChanged', route);
+				},
 
-                getChunks: function(){
-                    let self      = this;
-                    try{
-                        self.cid = this.$route.params.catID ;
-                    }catch (e) {
-                        self.cid = 1
-                    }
-                    self.cid = (!self.cid) ? 1 : self.cid;
+				getChunks: function(){
+					let self      = this;
+					try{
+						self.cid = this.$route.params.catID ;
+					}catch (e) {
+						self.cid = 1
+					}
+					self.cid = (!self.cid) ? 1 : self.cid;
 
-                    ajaxFetch.flatAjaxFetch(ajaxFetch.endPointBaseURL + ajaxFetch.prodByCIDEndPoint + self.cid, {}).then(
-                        function(response){
-                            if (response) {
-                                self.itemChunks = response;
-                                self.rendered   = true;
-                            }
-                        }).catch(function (error) {
-                        ajaxFetch.log(error.responseText);
-                    });
-                },
+					ajaxFetch.flatAjaxFetch(ajaxFetch.endPointBaseURL + ajaxFetch.prodByCIDEndPoint + self.cid, {}).then(
+						function(response){
+							if (response) {
+								self.itemChunks = [];
+								let temp        = [];
+								let counter     = 0;
+								for(let cue in response){
+									counter++;
+									temp.push(response[cue]);
+									if( (counter % self.columnsPerRow === 0) || counter === (response.length) ){
+										self.itemChunks.push(temp);
+										temp = [];
+									}
+								}
+								self.rendered   = true;
+							}
+						}).catch(function (error) {
+						ajaxFetch.log(error.responseText);
+					});
+				},
 
-            }
-        },
+			}
+		},
 
-    }
+	}
 </script>
 
 <style >
-  @import './../assets/css/products.css';
+    @import './../assets/css/products.css';
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,5 +1,5 @@
 <template>
-    <section  v-bind:class="getColumnClass()" v-if="wrapContent" style="min-height:520px;" >
+    <section  v-bind:class="getColumnClass()" v-if="wrapContent">
         <div class="pz-product-wrapper" :id="getBlockRootID(prodItem.productID)">
             <ItemTitleHeaderBlock :prodID="prodItem.productID"
                                   :prodTitle="prodItem.productTitle"
@@ -56,51 +56,51 @@
     </section>
 </template>
 
-  <script>
-      import ItemImageBlock             from '@/sub-components/ItemImageBlock';
-      import ItemPriceBlock             from '@/sub-components/ItemPriceBlock';
-      import ItemSocialsBlock           from '@/sub-components/ItemSocialsBlock';
-      import ItemQTBottomBlock          from '@/sub-components/ItemQTBottomBlock';
-      import ItemTitleHeaderBlock       from '@/sub-components/ItemTitleHeaderBlock';
-      import ItemAddOrRemoveBlock       from '@/sub-components/ItemAddOrRemoveBlock';
-      import ItemAttributesDataBlock    from '@/sub-components/ItemAttributesDataBlock';
-      import sMeths                     from "@/Services/SharedMethods";
-      export default {
-          name: "Item",
-          props: ['dataIndex', 'cue', 'colNum', 'wrapContent', 'prodItem'],
-          components: {
-              ItemPriceBlock,
-              ItemImageBlock,
-              ItemSocialsBlock,
-              ItemQTBottomBlock,
-              ItemTitleHeaderBlock,
-              ItemAddOrRemoveBlock,
-              ItemAttributesDataBlock,
-          },
+<script>
+	import ItemImageBlock             from '@/sub-components/ItemImageBlock';
+	import ItemPriceBlock             from '@/sub-components/ItemPriceBlock';
+	import ItemSocialsBlock           from '@/sub-components/ItemSocialsBlock';
+	import ItemQTBottomBlock          from '@/sub-components/ItemQTBottomBlock';
+	import ItemTitleHeaderBlock       from '@/sub-components/ItemTitleHeaderBlock';
+	import ItemAddOrRemoveBlock       from '@/sub-components/ItemAddOrRemoveBlock';
+	import ItemAttributesDataBlock    from '@/sub-components/ItemAttributesDataBlock';
+	import sMeths                     from "@/Services/SharedMethods";
+	export default {
+		name: "Item",
+		props: ['dataIndex', 'cue', 'colNum', 'wrapContent', 'prodItem'],
+		components: {
+			ItemPriceBlock,
+			ItemImageBlock,
+			ItemSocialsBlock,
+			ItemQTBottomBlock,
+			ItemTitleHeaderBlock,
+			ItemAddOrRemoveBlock,
+			ItemAttributesDataBlock,
+		},
 
-          data: function(){
-            return {
-                shoppingCartUrl : '/shopping-cart/basket',
-                restAccessURI : 'http://sim-rest.poiz.me/'
-            };
-          },
+		data: function(){
+			return {
+				shoppingCartUrl : '/shopping-cart/basket',
+				restAccessURI : 'http://sim-rest.poiz.me/'
+			};
+		},
 
-          methods : {
-              ...sMeths,
-              ...{
-                  getColumnClass : function(){
-                      this.colNum = (!this.colNum) ? 12: this.colNum;
-                      return 'col-xs-12 ' + 'col-md-' + this.colNum + ' col-lg-'+ this.colNum;
-                  },
+		methods : {
+			...sMeths,
+			...{
+				getColumnClass : function(){
+					this.colNum = (!this.colNum) ? 12: this.colNum;
+					return 'col-xs-12 ' + 'col-md-' + this.colNum + ' col-lg-'+ this.colNum;
+				},
 
-                  getBlockRootID : function(id, prefix){
-                      prefix = (undefined === prefix) ? '' : prefix;
-                      return prefix + 'pz-product-wrapper-' + id
-                  },
-              }
-          },
-      }
-  </script>
+				getBlockRootID : function(id, prefix){
+					prefix = (undefined === prefix) ? '' : prefix;
+					return prefix + 'pz-product-wrapper-' + id
+				},
+			}
+		},
+	}
+</script>
 
-  <style scoped>
-  </style>
+<style scoped>
+</style>
